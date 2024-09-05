6ca8a389dda15861a4e9c82d35b3c51f7ef9247b
harshjain262004 <harshjain262004@gmail.com>
2024-09-05 15:48:29 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template,redirect,url_for,flash,request,jsonify
+from db import *
+
+app = Flask(__name__)
+
+# base url
+@app.route('/')
+def index():
+    return redirect(url_for("signup"))
+
+# signup
+@app.route("/signup",methods=['POST','GET'])
+def signup():
+    if request.method == 'POST':
+        data = request.get_json()   
+        username = data.get('username')
+        password = data.get('password')
+        if add_signup(username,password):
+            flash("Account created successfully")
+            return redirect(url_for("login"))
+        else:
+            flash("Account already exists, Try logging in")
+            return render_template('login.html')
+    else:
+        return render_template('signup.html')
+
+# login
+@app.route("/login",methods=['POST','GET'])
+def login():
+    if request.method == 'POST':
+        useremail = request.form['username']
+        password = request.form['password']
+        if check_login(useremail,password):
+            flash("Succesful Login")
+            return redirect(url_for("dashboard"))
+        else:
+            flash("Incorrect credentials or account does not exist")
+            return render_template('login.html')
+    else:
+        return render_template('login.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ db.py
@@ -0,0 +1,7 @@
+def add_signup(useremail,password):
+    # add useremail and password in db for first time
+    pass
+
+def check_login(useremail,password):
+    # useremail already exists and fetch document and check
+    pass
