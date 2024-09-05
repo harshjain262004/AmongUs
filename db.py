efcda70cb3f409a7f55bc3670896059fa6cc573b
DevanshPaliwal <paliwal.devansh01@gmail.com>
2024-09-05 16:52:49 +0530

    Update db.py

--- db.py
+++ db.py
@@ -1,8 +1,31 @@
-def add_signup(useremail,password):
-    # add useremail and password in db for first time
-    pass
+from pymongo import MongoClient
+import hashlib
+
+
+connection_string="mongodb+srv://hjgoat:hjgoat@technormieshackx.jwvnz.mongodb.net/"
+client=MongoClient(connection_string)
+db=client["amongus"]
+userdata=db["userdata"]
+
+
+
+
+
+def add_signup(useremail="harsh12345@gmail.com",password="harshgoat12345",name="Harsh"):
+    newpass=hashlib.sha256(password.encode()).hexdigest()
+    query={"useremail":useremail}
+    if userdata.find_one(query):
+        return False
+    userdata.insert_one({'useremail':useremail,'password':newpass,'name':name})
+    return True
 
 def check_login(useremail,password):
-    # useremail already exists and fetch document and check
-    pass
+    newpass=hashlib.sha256(password.encode()).hexdigest()
+    query={"useremail":useremail,"password":newpass}
+    if userdata.find_one(query):
+        return True
+    return False
+
 #123
+
+# print(check_login(useremail="harsh12345@gmail.com",password="harshgoat123"))
