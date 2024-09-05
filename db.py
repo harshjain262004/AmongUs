7270fafcb8f1d76c6a660731872509cfa795b7b0
DevanshPaliwal <paliwal.devansh01@gmail.com>
2024-09-05 17:11:15 +0530

    Update db.py

--- db.py
+++ db.py
@@ -20,9 +20,15 @@ def add_signup(useremail,password,name):
     return True
 
 def check_login(useremail,password):
-    # useremail already exists and fetch document and check
-    pass
+    newpass=hashlib.sha256(password.encode()).hexdigest()
+    query={"useremail":useremail,"password":newpass}
+    if userdata.find_one(query):
+        return True
+    return False
 
-def getId(useremail,password):
-    #return string of id of useremail
-    pass
+def getName(useremail="harsh12345@gmail.com",password="harshgoat12345"):
+    newpass=hashlib.sha256(password.encode()).hexdigest()
+    query={"useremail":useremail,"password":newpass}
+    projection={"name":1,"_id":0}
+    result=userdata.find_one(query,projection)
+    return result['name']
